4ed34e0329dc2711aa255086257ccba910915092
Mike Schachter <mike.schachter@gmail.com>
2017-01-17 22:52:12 -0800

    refactor

--- voices/test/motor_programs.py
+++ voices/test/motor_programs.py
@@ -5,20 +5,60 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense, TimeDistributed
 from keras import backend as K
 
-def oscillator_control_activation(x):
-    u = K.relu(x[:, 0])
-    k = x[:, 1]
-    return K.concatenate([K.expand_dims(u), K.expand_dims(k)])
 
-def oscillator_control_loss(y_true, y_pred):
-    y_sqdiff = K.square(y_pred[:, 1:, :] - y_pred[:, :-1, :])
-    return K.mean(y_sqdiff)
+class MotorProgramRNN(object):
 
-n_batch = 2
+    def __init__(self):
+        pass
+
+    def activation_op(self):
+
+        def activation_func(x):
+            u = K.relu(x[:, 0])
+            k = x[:, 1]
+            return K.concatenate([K.expand_dims(u), K.expand_dims(k)])
+
+        return activation_func
+
+    def loss_op(self, alpha=1.0):
+
+        def loss_func(y_true, y_pred):
+            y_sqdiff = K.square(y_pred[:, 1:, :] - y_pred[:, :-1, :])
+            return alpha * K.mean(y_sqdiff)
+
+        return loss_func
+
+    def construct(self, num_units, num_time_points, num_inputs, num_outputs, alpha=1.0, optimizer='rmsprop'):
+
+        # construct the network
+        model = Sequential()
+        model.add(LSTM(num_units, return_sequences=True, input_shape=(num_time_points, num_inputs)))
+        model.add(TimeDistributed(Dense(num_outputs, activation=self.activation_op())))
+
+        # compile the network and specify loss, optimizer
+        model.compile(loss=self.loss_op(alpha), optimizer=optimizer)
+
+        self.model = model
+
+    def fit(self, Xtrain, Ytrain, Xtest, Ytest, batch_size=10, num_epochs=30):
+        # fit the data
+        self.model.fit(Xtrain, Ytrain, batch_size=batch_size, nb_epoch=num_epochs, validation_data=(Xtest, Ytest))
+
+    def predict(self, X):
+
+        assert len(X.shape) == 3, "X must be shape (batch_size, num_time_steps, num_input_dim)"
+        batch_size, num_time_steps, num_input_dim = X.shape
+
+        Ypred = self.model.predict(X, batch_size=batch_size)
+
+        return Ypred
+
+
+n_batch = 25
 n_in = 4
 n_unit = 5
 n_out = 2
-t_len = 10
+t_len = 50
 
 # set up sample data
 X_train = np.random.randn(n_batch, t_len, n_in)
@@ -27,19 +67,14 @@ Y_train = np.random.randn(n_batch, t_len, n_out)
 X_test = np.random.randn(n_batch, t_len, n_in)
 Y_test = np.random.randn(n_batch, t_len, n_out)
 
-# construct the network
-model = Sequential()
-model.add(LSTM(n_unit, return_sequences=True, input_shape=(t_len, n_in)))
-model.add(TimeDistributed(Dense(n_out, activation=oscillator_control_activation)))
+net = MotorProgramRNN()
+net.construct(n_unit, t_len, n_in, n_out, alpha=1.0)
+net.fit(X_train, Y_train, X_test, Y_test, batch_size=n_batch, num_epochs=20)
 
-# compile the network and specify loss, optimizer
-model.compile(loss=oscillator_control_loss, optimizer='rmsprop')
-
-# fit the data
-model.fit(X_train, Y_train,
-          batch_size=n_batch, nb_epoch=30,
-          validation_data=(X_test, Y_test))
-
-Y_pred = model.predict(X_test, batch_size=n_batch)
+Y_pred = net.predict(X_test)
 print 'Y_pred.shape=',Y_pred.shape
-print Y_pred
+
+# plt.figure()
+# ax = plt.subplot(2, 1, 1)
+# plt.plot(Y_pred)
+
